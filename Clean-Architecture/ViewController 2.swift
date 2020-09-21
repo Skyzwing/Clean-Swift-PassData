d8f38ef88aab88fc3aadfe5aaecfa3c558afba4c
Surachet Yaitammasan <aottapkub@hotmail.com>
2020-09-21 15:27:03 +0700

    Clean Swift

---
+++ Clean-Architecture/ViewController 2.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clean-Architecture
+//
+//  Created by Surachat Yaitammasan on 15/9/2563 BE.
+//  Copyright © 2563 Surachat Yaitammasan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
